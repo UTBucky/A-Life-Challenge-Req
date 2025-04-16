94194489031db5f0af5bcc90b5a904b0a0f86502
Paris Yikang Zhou <122337549+zhouparis@users.noreply.github.com>
2025-04-16 12:13:49 -0700

    Update grid_viewer.py
    Flake8 linting

--- grid_viewer.py
+++ grid_viewer.py
@@ -4,38 +4,42 @@
 
 import pygame
 
+
 class GridViewer:
     """
     Pygames rendering for nxn environment
     """
-    
     def __init__(self, environment, tile_size=10, sidebar_width=200):
         """
-        Stores an a-life environment to render, tilesize and sidebar are default 10/200 respectively
+        Stores an a-life environment to render, tilesize and sidebar are
+        default 10/200 respectively
         """
         self.env = environment
         self.tile_size = tile_size
         self.sidebar_width = sidebar_width
 
-        # make the windows size 
-        self.window_size = (environment.get_size() * tile_size + sidebar_width, environment.get_size() * tile_size)
+        # make the windows size
+        self.window_size = (
+            environment.get_size() * tile_size + sidebar_width,
+            environment.get_size() * tile_size
+            )
         pygame.init()
 
         # choose what to display
         self.screen = pygame.display.set_mode(self.window_size)
-        
+
         # Window caption, at the very very tippy top, might change to title
         pygame.display.set_caption("A-Life Grid Viewer")
 
         # pygames var for rendering, keeps track of time
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont("consolas",20)
+        self.font = pygame.font.SysFont("consolas", 20)
 
     def draw(self):
         """
         Render the environment
         """
-        
+
         ts = self.tile_size
         sw = self.sidebar_width
         size = self.env.get_size()
@@ -60,10 +64,15 @@ class GridViewer:
             )
 
         # Draw the screen
-        pygame.draw.rect(self.screen, (30, 30, 30), pygame.Rect(0, 0, sw, size * ts))
+        pygame.draw.rect(
+            self.screen, (30, 30, 30),
+            pygame.Rect(0, 0, sw, size * ts)
+            )
 
         # Generation counter
-        gen_text = self.font.render(f"Generation: {self.env.get_generation()}", True, (255, 255, 255))
+        gen_text = self.font.render(
+            f"Generation: {self.env.get_generation()}", True, (255, 255, 255)
+            )
         self.screen.blit(gen_text, (10, 10))
 
         # continue rendering

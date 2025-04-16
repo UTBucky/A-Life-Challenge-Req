eac800f17b9a802958c84caff67bd06e3d5e3931
Paris Yikang Zhou <122337549+zhouparis@users.noreply.github.com>
2025-04-16 12:13:20 -0700

    Update grid_environment.py
    Flake8 linting

--- grid_environment.py
+++ grid_environment.py
@@ -4,21 +4,23 @@
 
 import numpy as np
 
+
 class GridEnvironment:
     """
-    GridEnvironment(1 arg) 
+    GridEnvironment(1 arg)
     size: size of grid desired
-    uses 2 different np arrays of size (size x size) for tracking occupancy and terrain - filled with 0s - dtype uint8 
+    2 different np arrays of size (size x size) terrain and occupancy
+    - filled with 0s - dtype uint8
     organisms are stored in a python list datastructure
     """
-    
+
     def __init__(self, size):
         """
         np arrays for terrain and occupancy
-        python list for organisms 
+        python list for organisms
         """
         self._size = size
-        self.terrain = np.zeros((size, size), dtype=np.uint8)  # 0 = land, 1 = water
+        self.terrain = np.zeros((size, size), dtype=np.uint8)
         self.occupancy = np.zeros((size, size), dtype=bool)
         self._organisms = []  # List of organism instances
         self._generation = 0
@@ -26,14 +28,12 @@ class GridEnvironment:
     def add_organism(self, organism):
         """
         Environment method that takes 1 arg
-        organism: takes a custom class that contains genes and a method for moving
-        r, c are rows/columns 
+        organism: inp custom class that contains genes and a method for moving
+        r, c are rows/columns
         """
         r, c = organism.position
-       
-        #Checks created organism is in bounds of board 
-        #and not within the occupancy array
-        
+        # Checks created organism is in bounds of board
+        # and not within the occupancy array
         if self.in_bounds(r, c) and not self.occupancy[r, c]:
             self._organisms.append(organism)
             self.occupancy[r, c] = True
@@ -41,8 +41,8 @@ class GridEnvironment:
         return False
 
     def in_bounds(self, r, c):
-        """ 
-        Checks organism is within the board size 
+        """
+        Checks organism is within the board size
         """
         return 0 <= r < self._size and 0 <= c < self._size
 
@@ -52,16 +52,18 @@ class GridEnvironment:
         """
         # Build speed-sorted queue (descending order: faster goes first)
         # key=lambda o: o.speed >>> tells inbult python sorting method sorted()
-        # to use the speed method to sort (this documentation created before 
+        # to use the speed method to sort (this documentation created before
         # full build organism was created)
-        move_queue = sorted(self._organisms, key=lambda o: o.speed, reverse=True)
+        move_queue = sorted(
+            self._organisms, key=lambda o: o.speed, reverse=True
+            )
 
         # Track tiles visited this step to avoid collisions
-        # zeroes_like creates an np array initialized with 0s of same shape 
+        # zeroes_like creates an np array initialized with 0s of same shape
         # as inputted array, in this case it's the occupancy array
         visited = np.zeros_like(self.occupancy, dtype=bool)
 
-        # --- If we can avoid python for loop in future we should, come back to this ---
+        # ---If possible avoid python for loop in future
         for organism in move_queue:
             r_old, c_old = organism.position
 
@@ -96,13 +98,13 @@ class GridEnvironment:
         Returns generation for display in pygames rendering
         """
         return self._generation
-    
+
     def get_size(self):
         """
         Returns size for rendering in pygames draw method
         """
         return self._size
-    
+
     def get_organisms(self):
         """
         Returns organisms for rendering in pygames
